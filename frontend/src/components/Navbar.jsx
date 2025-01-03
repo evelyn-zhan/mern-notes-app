a56de3b2cc1e2bd97bcc97d46311a5cef33dca61
Evelyn <evelynzanpx@gmail.com>
2025-01-03 13:56:33 +0700

    update: search bar states and handler functions

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,18 +1,33 @@
-import React from 'react'
+import React, { useState} from 'react'
 import { useNavigate } from 'react-router-dom'
 
+import SearchBar from '../components/SearchBar.jsx'
 import ProfileInfo from '../components/ProfileInfo.jsx'
 
 function Navbar() {
+    const [searchQuery, setSearchQuery] = useState("")
     const navigate = useNavigate()
     
     const onLogOutHandler = () => {
         navigate("/login")
     }
 
+    const onSearchChangeHandler = (event) => {
+        setSearchQuery(event.target.value)
+    }
+
+    const onSearchHandler = () => {
+        
+    }
+
+    const onClearHandler = () => {
+        setSearchQuery("")
+    }
+
     return (
         <div className="px-8 py-4 flex items-center justify-between bg-white drop-shadow">
             <h2 className="text-xl font-medium text-black">Notes App</h2>
+            <SearchBar value={searchQuery} onSearchChangeHandler={onSearchChangeHandler} onSearchHandler={onSearchHandler} onClearHandler={onClearHandler} />
             <ProfileInfo onLogOutHandler={onLogOutHandler} />
         </div>
     )
--- frontend/src/components/SearchBar.jsx
+++ frontend/src/components/SearchBar.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
-function SearchBar() {
+function SearchBar({ value, onSearchChangeHandler, onSearchHandler, onClearHandler }) {
     return (
-        <div>SearchBar</div>
+        <div className="w-80 px-4 flex items-center bg-slate-100 rounded-md">
+            <input type="text" placeholder="Search notes" value={value} onChange={onSearchChangeHandler} className="w-full py-[11px] text-xs bg-transparent outline-none" />
+            { value && <i className="ri-close-line mr-3 text-xl text-slate-500 hover:text-black cursor-pointer" onClick={onClearHandler}></i> }
+            <i className="ri-search-line text-slate-400 hover:text-black cursor-pointer" onClick={onSearchHandler}></i>
+        </div>
     )
 }
 
