fb96df222c3fe2f726e50f0d8d0afb3f4ea7633f
Evelyn <evelynzanpx@gmail.com>
2025-01-03 13:20:51 +0700

    add: profile info on navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,9 +1,19 @@
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
+
+import ProfileInfo from '../components/ProfileInfo.jsx'
 
 function Navbar() {
+    const navigate = useNavigate()
+    
+    const onLogOutHandler = () => {
+        navigate("/login")
+    }
+
     return (
         <div className="px-8 py-4 flex items-center justify-between bg-white drop-shadow">
             <h2 className="text-xl font-medium text-black">Notes App</h2>
+            <ProfileInfo onLogOutHandler={onLogOutHandler} />
         </div>
     )
 }
