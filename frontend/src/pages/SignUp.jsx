451b62c6268bbc1f36a8920f4e5b9d779972e367
Evelyn <evelynzanpx@gmail.com>
2025-01-03 12:55:29 +0700

    update: sign up page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -1,6 +1,10 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 
 import Navbar from '../components/Navbar.jsx'
+import PasswordInput from '../components/PasswordInput.jsx'
+
+import { validateEmail } from '../utils/validate.js'
 
 function SignUp() {
     const [name, setName] = useState("")
@@ -16,9 +20,31 @@ function SignUp() {
         setEmail(event.target.value)
     }
 
+    const onPasswordChangeHandler = (event) => {
+        setPassword(event.target.value)
+    }
+
     const onSignUpHandler = async (event) => {
         event.preventDefault()
 
+        if (!name) {
+            setError("Please enter your name.")
+            return
+        }
+
+        if (!validateEmail(email)) {
+            setError("Please enter a valid e-mail address.")
+            return
+        }
+                
+        if (!password) {
+            setError("Please enter the password.")
+            return
+        }
+        
+        setError(null)
+        
+        // Sign Up API
     }
 
     return (
@@ -32,6 +58,16 @@ function SignUp() {
 
                         <input type="text" placeholder="Name" value={name} onChange={onNameChangeHandler} className="input-box" />
                         <input type="text" placeholder="E-mail" value={email} onChange={onEmailChangeHandler} className="input-box" />
+                        <PasswordInput value={password} onPasswordChangeHandler={onPasswordChangeHandler} />
+
+                        { error && <p className="pb-1 text-red-500 text-xs">{error}</p> }
+
+                        <button type="submit" className="btn-primary">Create Account</button>
+                        
+                        <p className="text-sm text-center mt-4">
+                            Already have an account?{" "}
+                            <Link to="/login" className="text-primary font-medium underline">Login</Link>
+                        </p>
                     </form>
                 </div>
             </div>
