7d531bfd1d5ca31f17e1f4a396485e8c06a35ad0
Evelyn <evelynzanpx@gmail.com>
2025-01-03 14:12:55 +0700

    update: home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
 
 import Navbar from '../components/Navbar.jsx'
+import NoteCard from '../components/NoteCard.jsx'
 
 function Home() {
     return (
         <>
             <Navbar />
+
+            <div className="container mx-auto">
+                <NoteCard title="Meeting with the team" />
+            </div>
         </>
     )
 }
