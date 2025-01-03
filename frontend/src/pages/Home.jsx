00429accd708f10c7e37c2a668efa2f42b5e4e15
Evelyn <evelynzanpx@gmail.com>
2025-01-03 13:21:02 +0700

    update: home page

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
 
+import Navbar from '../components/Navbar.jsx'
+
 function Home() {
     return (
-        <div>Home</div>
+        <>
+            <Navbar />
+        </>
     )
 }
 
